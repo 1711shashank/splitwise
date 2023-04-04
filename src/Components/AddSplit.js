271712cb4406ff6366cf9865715376d59d3635d3
1711shashank <1711shashank@gmail.com>
2023-04-05 00:00:08 +0530

    splitBetween varible

--- src/Components/AddSplit.js
+++ src/Components/AddSplit.js
@@ -1,16 +1,18 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import ListItem from '@mui/material/ListItem';
+import axios from 'axios';
 import { Button } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import SplitBetweenProfiles from '../Layout/SplitBetweenProfiles'
 import './AddSplit.css'
 import uniqid from 'uniqid'
-import axios from 'axios'
 
 
-const AddSplit = (props) => {
-    
+function AddSplit() {
+
     const [amount, setAmount] = useState(0);
     const [message, setMessage] = useState('');
+    const [selectedMember, setSelectedMember] = useState([]);
     const [inboxMemberArray, setInboxMemberArray] = useState([]);
 
     const navigate = useNavigate();
@@ -19,7 +21,8 @@ const AddSplit = (props) => {
 
     const fetchData = async () => {
         const response = await axios.post(`http://localhost:5000/getMessages`, { authToken: localStorage.getItem('authToken'), inboxId: localStorage.getItem('inboxId') });
-        setInboxMemberArray(response.data.inboxData.inboxMember)
+        setInboxMemberArray(response.data.inboxData.inboxMember);
+        console.log(response.data.inboxData.inboxMember);
     };
 
     useEffect(() => {
@@ -29,16 +32,24 @@ const AddSplit = (props) => {
         fetchData();
     }, [])
 
+    const handleToggle = (emailId) => () => {
+        if (selectedMember.includes(emailId)) {
+            setSelectedMember(selectedMember.filter((email) => email !== emailId));
+        } else {
+            setSelectedMember([...selectedMember, emailId]);
+        }
+    };
+
     const handalClickSplitButton = async () => {
 
         const newMessage = {
-            inboxId : localStorage.getItem('inboxId'),
-            messageCardId : uniqid(),
-            amount: amount, 
+            inboxId: localStorage.getItem('inboxId'),
+            messageCardId: uniqid(),
+            amount: amount,
             date: new Date().toString(),
-            message: message, 
+            message: message,
             senderName: 'senderName',
-            splitBetween:['gyuu','aksjdh','wkjhew']
+            splitBetween: selectedMember
         }
 
         await axios.post(`http://localhost:5000/sentMessage`, { authToken: localStorage.getItem('authToken'), newMessage });
@@ -50,7 +61,6 @@ const AddSplit = (props) => {
     return (
         <>
             <div className='AddSplit'>
-
                 <div className='AddSplit-details'>
                     <p className='AddSplit-text'>Total</p>
                     <input className='AddSplit-total' type='number' placeholder='0' ref={emailInput} onChange={(e) => setAmount(e.target.value)} />
@@ -59,9 +69,21 @@ const AddSplit = (props) => {
 
                 <div className='AddSplit-profiles'>
                     {inboxMemberArray.map((curInboxMember) => (
-                        <div key={uniqid()}>
-                            <SplitBetweenProfiles inboxMember={curInboxMember} contributionAmount={amount / numberOfMembers} />
-                        </div>
+                        <ListItem className='AddSplit-SplitBetweenProfiles' key={curInboxMember.email} dense button onClick={handleToggle(curInboxMember.email)}>
+                            <div className='AddSplit-SplitBetweenProfiles-left'>
+                                <Checkbox checked={selectedMember.includes(curInboxMember.email)} />
+                                <div className='ChatCard-avatar'>
+                                    <img src='https://static.vecteezy.com/system/resources/previews/006/487/917/original/man-avatar-icon-free-vector.jpg' width='35px' height='35px' alt='' />
+                                </div>
+                                <p className='AddSplit-profile-name'> {curInboxMember.name} </p>
+                            </div>
+
+                            <div className='AddSplit-SplitBetweenProfiles-right'>
+                                <p style={{ fontSize: '16px', width: 80, color: 'white', padding: '0 10px' }}>
+                                    {amount / numberOfMembers}
+                                </p>
+                            </div>
+                        </ListItem>
                     ))}
                 </div>
 
@@ -70,7 +92,12 @@ const AddSplit = (props) => {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default AddSplit
+export default AddSplit;
+
+
+
+
+
